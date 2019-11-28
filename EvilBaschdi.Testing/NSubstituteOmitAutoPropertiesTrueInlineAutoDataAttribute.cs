b3e2a95e2cce901b73e26dc2f69174dad3665d3e
Sebastian Walter <sebwalter@gmx.net>
2019-11-28 20:07:53 +0100

    added InlineAutoDataAttribute, updated Nuget Packages

---
+++ EvilBaschdi.Testing/NSubstituteOmitAutoPropertiesTrueInlineAutoDataAttribute.cs
@@ -0,0 +1,17 @@
+﻿using AutoFixture.Xunit2;
+
+namespace EvilBaschdi.Testing
+{
+    /// <inheritdoc />
+    public class NSubstituteOmitAutoPropertiesTrueInlineAutoDataAttribute : InlineAutoDataAttribute
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="values"></param>
+        public NSubstituteOmitAutoPropertiesTrueInlineAutoDataAttribute(params object[] values)
+            : base(new NSubstituteOmitAutoPropertiesTrueAutoDataAttribute(), values)
+        {
+        }
+    }
+}
