3f9131078fd8d08d9815642fd11dec65d0b6b688
evilbaschdi <sebwalter@gmx.net>
2022-02-13 09:58:30 +0100

    updated nuget packages and switched to file-scoped namespace

--- EvilBaschdi.Testing/NSubstituteOmitAutoPropertiesTrueInlineAutoDataAttribute.cs
+++ EvilBaschdi.Testing/NSubstituteOmitAutoPropertiesTrueInlineAutoDataAttribute.cs
@@ -1,18 +1,17 @@
 ﻿using AutoFixture.Xunit2;
 
-namespace EvilBaschdi.Testing
+namespace EvilBaschdi.Testing;
+
+/// <inheritdoc />
+// ReSharper disable once UnusedType.Global
+public class NSubstituteOmitAutoPropertiesTrueInlineAutoDataAttribute : InlineAutoDataAttribute
 {
-    /// <inheritdoc />
-    // ReSharper disable once UnusedType.Global
-    public class NSubstituteOmitAutoPropertiesTrueInlineAutoDataAttribute : InlineAutoDataAttribute
+    /// <summary>
+    ///     Constructor
+    /// </summary>
+    /// <param name="values"></param>
+    public NSubstituteOmitAutoPropertiesTrueInlineAutoDataAttribute(params object[] values)
+        : base(new NSubstituteOmitAutoPropertiesTrueAutoDataAttribute(), values)
     {
-        /// <summary>
-        ///     Constructor
-        /// </summary>
-        /// <param name="values"></param>
-        public NSubstituteOmitAutoPropertiesTrueInlineAutoDataAttribute(params object[] values)
-            : base(new NSubstituteOmitAutoPropertiesTrueAutoDataAttribute(), values)
-        {
-        }
     }
 }
