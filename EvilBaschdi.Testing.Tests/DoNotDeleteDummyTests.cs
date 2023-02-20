7de9a3f717d49d5b43660e942f378baaecf336ed
Sebastian Walter <sebwalter@gmx.net>
2023-02-20 08:42:22 +0100

    updated xunit packages to 2.4.3-pre.5

--- EvilBaschdi.Testing.Tests/DoNotDeleteDummyTests.cs
+++ EvilBaschdi.Testing.Tests/DoNotDeleteDummyTests.cs
@@ -1,21 +1,18 @@
-using Xunit;
+namespace EvilBaschdi.Testing.Tests;
 
-namespace EvilBaschdi.Testing.Tests
+/// <summary>
+///     Do not delete this class.
+///     NCrunch needs at least one test method in witch <see cref="Assert" />.Equal() is used.
+/// </summary>
+// ReSharper disable once TestFileNameWarning
+public class DoNotDeleteDummyTests
 {
-    /// <summary>
-    ///     Do not delete this class.
-    ///     NCrunch needs at least one test method in witch <see cref="Assert" />.Equal() is used.
-    /// </summary>
-    // ReSharper disable once TestFileNameWarning
-    public class DoNotDeleteDummyTests
+    [Fact]
+    public void Value_ToEnableUnitTests_Asserts1Equals1()
     {
-        [Fact]
-        public void Value_ToEnableUnitTests_Asserts1Equals1()
-        {
-            // Arrange
-            // Act
-            // Assert
-            Assert.Equal(1, 1);
-        }
+        // Arrange
+        // Act
+        // Assert
+        Assert.Equal(1, 1);
     }
 }
