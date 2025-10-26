d3ed1a78fd4657bcbf5dfc7feb2a3e8e39dc67a3
Sebastian Walter <sebwalter@gmx.net>
2025-10-26 10:37:57 +0100

    Added EvilBaschdi.Testing.Avalonia

---
+++ EvilBaschdi.Testing.Avalonia/AvaloniaTestBase.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia;
+
+namespace EvilBaschdi.Testing.Avalonia;
+
+/// <summary>
+///     Base class for Avalonia tests that ensures the headless platform is initialized
+/// </summary>
+public abstract class AvaloniaTestBase<TApp>
+    where TApp : Application, new()
+{
+    /// <summary>
+    ///     Constructor
+    /// </summary>
+    protected AvaloniaTestBase()
+    {
+        TestAppBuilder<TApp>.EnsureInitialized();
+    }
+}
